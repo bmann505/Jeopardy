285d49d3657a9d87b4b07f6444d628a7ec0b03ff
Brian Mann <bmann505@gamil.com>
2017-07-05 12:08:59 -0600

    random categories function

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ $(document).ready(function() {
 
 
 })
+
+function getCategories() {
+  $.get('https://opentdb.com/api_category.php')
+    .then(function(data) {
+      var categoryArray = [];
+      var randomArray = [];
+      for (var i = 0; i < data.trivia_categories.length; i++) {
+        categoryArray.push(data.trivia_categories[i].id)
+      }
+      while (randomArray.length < 6) {
+        var randomNum = Math.floor(Math.random() * 24);
+        if (randomArray.indexOf(categoryArray[randomNum]) == -1) {
+          randomArray.push(categoryArray[randomNum]);
+        }
+      }
+    })
+}
+
+getCategories();
