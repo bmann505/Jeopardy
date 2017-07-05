4555cbba9e49e406f90837c6f84586c0c0a84e22
Brian Mann <bmann505@gamil.com>
2017-07-05 14:44:15 -0600

    dynamic titles

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
+  $titleOne = $('.title-one');
+  $titleTwo = $('.title-two');
+  $titleThree = $('.title-three');
+  $titleFour = $('.title-four');
+  $titleFive = $('.title-five');
+  $titleSix = $('.title-six');
 
-
+  getCategories();
 })
+
+var categoryArray = [];
+var randomArray = [];
 
 function getCategories() {
   $.get('https://opentdb.com/api_category.php')
     .then(function(data) {
-      var categoryArray = [];
-      var randomArray = [];
       for (var i = 0; i < data.trivia_categories.length; i++) {
         categoryArray.push(data.trivia_categories[i].id)
       }
@@ -17,7 +24,35 @@ function getCategories() {
           randomArray.push(categoryArray[randomNum]);
         }
       }
+      var urlOne = 'https://opentdb.com/api.php?amount=6&category=' + randomArray[0] + '&type=multiple';
+      var urlTwo = 'https://opentdb.com/api.php?amount=6&category=' + randomArray[1] + '&type=multiple';
+      var urlThree = 'https://opentdb.com/api.php?amount=6&category=' + randomArray[2] + '&type=multiple';
+      var urlFour = 'https://opentdb.com/api.php?amount=6&category=' + randomArray[3] + '&type=multiple';
+      var urlFive = 'https://opentdb.com/api.php?amount=6&category=' + randomArray[4] + '&type=multiple';
+      var urlSix = 'https://opentdb.com/api.php?amount=6&category=' + randomArray[5] + '&type=multiple';
+      $.get(urlOne)
+        .then(function(dataOne) {
+          $titleOne.text(dataOne.results[0].category)
+        })
+      $.get(urlTwo)
+        .then(function(dataTwo) {
+          $titleTwo.text(dataTwo.results[0].category);
+        })
+      $.get(urlThree)
+        .then(function(dataThree) {
+          $titleThree.text(dataThree.results[0].category);
+        })
+      $.get(urlFour)
+        .then(function(dataFour) {
+          $titleFour.text(dataFour.results[0].category);
+        })
+      $.get(urlFive)
+        .then(function(dataFive) {
+          $titleFive.text(dataFive.results[0].category);
+        })
+      $.get(urlSix)
+        .then(function(dataSix) {
+          $titleSix.text(dataSix.results[0].category);
+        })
     })
 }
-
-getCategories();
